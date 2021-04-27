5cd0da4087e621610fe220a47d6eabe7b0d8bac1
MORGAN Olivier <olivier.morgan@ec.europa.eu>
2021-04-27 14:33:22 +0200

    .

--- meyniej/SailTest1/110418100429/RESULT/romcV2_subExec.bat
+++ meyniej/SailTest1/110418100429/RESULT/romcV2_subExec.bat
@@ -1,4 +1,5 @@
-.r /home/robusmo/projects/ROMC/USERS_pyV2/CODE/IDL/RAMI_MODEL_TESTER.pro
+;.r /home/robusmo/projects/ROMC/USERS_pyV2/CODE/IDL/RAMI_MODEL_TESTER.pro
+.r /home/morgaol/ROMC/IDL/RAMI_MODEL_TESTER.pro
 
 set_plot,'NULL'
 exp1=['HOM01_TUR_ERE_NR1_00','HOM01_TUR_ERE_NR1_30','HOM01_TUR_ERE_NR1_60']
@@ -8,7 +9,11 @@ MEASLIST=['brfpp']
 USR_INDIR=['/home/robusmo/projects/ROMC/USERS_pyV2/meyniej/SailTest1/110418100429/DATA/']
 REF_INDIR='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/REFERENCE_DATA/FORWARD/DEBUG/RAMI3/HOM/'
 PYOUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/meyniej/SailTest1/110418100429/RESULT/'
-OUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/meyniej/SailTest1/110418100429/RESULT/'
+
+;OUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/meyniej/SailTest1/110418100429/RESULT/'
+OUTDIR='/home/lancoch/sharejrcbox/olivier/ROMC_Results/meyniej/SailTest1/110418100429/IDL/'
+PYOUTDIR='/home/lancoch/sharejrcbox/olivier/ROMC_Results/meyniej/SailTest1/110418100429/python/'
+
 VALEXPFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_EXPERIMENTS_Types.txt'
 VALMESFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_MEASUREMENTS_Types.txt'
 REFTAG='SailTest1_110418100429'
