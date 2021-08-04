02ae9f3a049f0a9a9bbc4ac8b4635c083db3d479
Your Name <you@example.com>
2021-08-04 18:59:25 +0200

    .

---
+++ abdelaziz/PolVRT/260116145033/romcV2_subExec.bat
@@ -0,0 +1,24 @@
+.r /home/robusmo/projects/ROMC/USERS_pyV2/CODE/IDL/RAMI_MODEL_TESTER.pro
+
+set_plot,'NULL'
+exp1=['HET01_DIS_UNI_NR1_20']
+
+MEASLIST=['brfop_mlt','brfpp_mlt','brfop','brfpp_co_sgl','brfop_co_sgl','brfpp','brfop_uc_sgl','brfpp_uc_sgl']
+
+USR_INDIR=['/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/DATA/']
+REF_INDIR='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/REFERENCE_DATA/FORWARD/DEBUG/RAMI3/HET/'
+PYOUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/RESULT/'
+OUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/RESULT/'
+VALEXPFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_EXPERIMENTS_Types.txt'
+VALMESFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_MEASUREMENTS_Types.txt'
+REFTAG='PolVRT_260116145033'
+REFMODNM='ROMCREF'
+MODELNM=['PolVRT']
+WATERMRK='DEBUG'
+
+exp=[exp1]
+
+
+RAMI_MODEL_TESTER,exp,MEASLIST,MODELNM,watermark=WATERMRK,/prps,USR_INDIR=USR_INDIR,REF_INDIR=REF_INDIR,OUTDIR=OUTDIR,VALEXPFILE=VALEXPFILE,VALMESFILE=VALMESFILE,REFTAG=REFTAG,REFMODNM=REFMODNM,enveloppe=[1.0,2.5,5.0],/rms,/nobwjpeg,/skill,/snr
+
+exit
---
+++ meyniej/dartTest1/110518150718/romcV2_subExec.bat
@@ -0,0 +1,24 @@
+.r /home/robusmo/projects/ROMC/USERS_pyV2/CODE/IDL/RAMI_MODEL_TESTER.pro
+
+set_plot,'NULL'
+exp1=['HOM01_TUR_ERE_NR1_00']
+
+MEASLIST=['brfpp_mlt','brfop_uc_sgl','brfpp_co_sgl','brfop','brfop_mlt','brfpp_uc_sgl','brfop_co_sgl','brfpp']
+
+USR_INDIR=['/home/robusmo/projects/ROMC/USERS_pyV2/meyniej/dartTest1/110518150718/DATA/']
+REF_INDIR='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/REFERENCE_DATA/FORWARD/DEBUG/RAMI3/HOM/'
+PYOUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/meyniej/dartTest1/110518150718/RESULT/'
+OUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/meyniej/dartTest1/110518150718/RESULT/'
+VALEXPFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_EXPERIMENTS_Types.txt'
+VALMESFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_MEASUREMENTS_Types.txt'
+REFTAG='dartTest1_110518150718'
+REFMODNM='ROMCREF'
+MODELNM=['dartTest1']
+WATERMRK='DEBUG'
+
+exp=[exp1]
+
+
+RAMI_MODEL_TESTER,exp,MEASLIST,MODELNM,watermark=WATERMRK,/prps,USR_INDIR=USR_INDIR,REF_INDIR=REF_INDIR,OUTDIR=OUTDIR,VALEXPFILE=VALEXPFILE,VALMESFILE=VALMESFILE,REFTAG=REFTAG,REFMODNM=REFMODNM,enveloppe=[1.0,2.5,5.0],/rms,/nobwjpeg,/skill,/snr
+
+exit
