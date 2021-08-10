9de87f52e1e1cce8ff4535f2d1c3014cf65c5e36
MORGAN Olivier <olivier.morgan@ec.europa.eu>
2021-08-11 00:51:57 +0200

    .

--- COMPARISON/100821140850/romc_params.bat
+++ COMPARISON/100821140850/romc_params.bat
@@ -6,7 +6,12 @@ REFTAG='COMPARE_100821140850'
 USR_INDIR=['/home/robusmo/projects/ROMC/USERS_pyV2/robusmo/COMPARISON/100821140850/input/tete/100821144704/DATA/','/home/robusmo/projects/ROMC/USERS_pyV2/robusmo/COMPARISON/100821140850/input/prova/100821144505/DATA/']
 MODELNM=['tete','prova']
 REF_INDIR='/home/robusmo/projects/ROMC/USERS_pyV2/robusmo/COMPARISON/100821140850/input/lollo/100821144157/DATA/'
+
 OUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/robusmo/COMPARISON/100821140850/'
+OUTDIR='/home/morgaol/jrcbox_staging/ROMC_Results/COMPARISON/100821140850/IDL/'
+PYOUTDIR='/home/morgaol/jrcbox_staging/ROMC_Results/COMPARISON/100821140850/python/'
+
+
 VALEXPFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_MEASUREMENTS_Types.txt'
 VALMESFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_EXPERIMENTS_Types.txt'
 REFMODNM='lollo'
