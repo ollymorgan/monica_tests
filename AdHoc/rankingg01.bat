6c60a43145957eaec0ef5ee10a0246aa5e60c917
MORGAN Olivier <olivier.morgan@ec.europa.eu>
2021-08-10 12:05:24 +0200

    .

--- AdHoc/rankingg01.bat
+++ AdHoc/rankingg01.bat
@@ -2,31 +2,32 @@
 .r /home/morgaol/ROMC/IDL/ROMC_MODEL_RANKS.pro
 
 set_plot,'NULL'
-exp1=['HET01_DIS_UNI_NR1_20']
 
 MEASLIST=['brfop_mlt','brfpp_mlt','brfop','brfpp_co_sgl','brfop_co_sgl','brfpp','brfop_uc_sgl','brfpp_uc_sgl']
 
 USR_INDIR=['/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/DATA/']
 REF_INDIR='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/REFERENCE_DATA/FORWARD/DEBUG/RAMI3/HET/'
 
-PYOUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/RESULT/'
-OUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/RESULT/'
+;PYOUTDIR='/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/RESULT/'
+RESDIR0='/home/robusmo/projects/ROMC/USERS_pyV2/abdelaziz/PolVRT/260116145033/RESULT/'
+RESDIR1='/home/robusmo/projects/ROMC/USERS_pyV2/robusmo/RANKING/example/meyniej/dartTest1/110518150718/RESULT/'
 
-PYOUTDIR='/home/morgaol/jrcbox_staging/ROMC_Results/abdelaziz/PolVRT/260116145033/python/'
-OUTDIR='/home/morgaol/jrcbox_staging/ROMC_Results/abdelaziz/PolVRT/260116145033/IDL/'
+;PYOUTDIR='/home/morgaol/jrcbox_staging/ROMC_Results/abdelaziz/PolVRT/260116145033/python/'
+;OUTDIR='/home/morgaol/jrcbox_staging/ROMC_Results/abdelaziz/PolVRT/260116145033/IDL/'
+OUTDIR = '/home/lancoch/sharejrcbox/olivier/AdHoc_Ranking/'
 
 VALEXPFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_EXPERIMENTS_Types.txt'
 VALMESFILE='/home/robusmo/projects/ROMC/USERS_pyV2/DATA/ROFC/ValidFiles/FORWARD/DEBUG/RAMI3/Valid_MEASUREMENTS_Types.txt'
-REFTAG='PolVRT_260116145033'
+
+REFTAG='Test_01'
 REFMODNM='ROMCREF'
-MODELNM=['PolVRT']
+MODELNM=['PolVRT', 'dartTest1']
 WATERMRK='DEBUG'
 
-exp=[exp1]
 
 rankfilnm='skill.txt'
 
-skilldir=[OUTDIR]
+skilldir=[RESDIR0, RESDIR1]
 ROMC_MODEL_RANKS,skilldir,MEASLIST,modelnm,REFTAG=reftag,OUTDIR=outdir,RANKFILNM=rankfilnm,VALMESFILE=valmesfile,watermark='DEBUG',/prps
 
 
