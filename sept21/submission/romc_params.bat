f04e20d6d1190c060ede55e8dbaf170f59e3ea02
MORGAN Olivier <olivier.morgan@ec.europa.eu>
2021-09-13 14:17:00 +0200

    .

--- sept21/submission/romc_params.bat
+++ sept21/submission/romc_params.bat
@@ -1,5 +1,5 @@
 ;
-.r /home/robusmo/projects/ROMC/USERS_pyV2/CODE/IDL/RAMI_MODEL_TESTER.pro
+.r /home/robusmo/projects/ROMC/USERS_idl/input/RAMI_MODEL_TESTER.pro
 ;.r /home/morgaol/ROMC/IDL/RAMI_MODEL_TESTER.pro
 
 set_plot,'NULL'
